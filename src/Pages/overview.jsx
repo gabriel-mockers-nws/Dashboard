f61139c11da778c173d66d7a9ee07697aa5d5d25
gabriel-mockers-nws <gmockers@normandiewebschool.fr>
2025-09-04 16:47:58 +0200

    Add responsive

--- src/Pages/overview.jsx
+++ src/Pages/overview.jsx
@@ -8,7 +8,7 @@ export const Overview = () => {
                 <h1 className="text-4xl font-bold text-center py-5">Vue d&apos;ensemble</h1>
             </div>
             <h1 className="text-4xl font-bold text-center py-5 text-white">Ventes premier semestre 2024</h1>
-            <div className="flex justify-around px-5">
+            <div className="md:flex md:justify-around px-5 grid grid-cols-1 gap-10">
                 <Chartcam />
 
                 <ChartGraph />

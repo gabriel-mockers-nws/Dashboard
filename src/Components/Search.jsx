a269291cb71fa739dacf37baab2949e31e9c239c
gabriel-mockers-nws <gmockers@normandiewebschool.fr>
2025-09-11 10:53:44 +0200

    Creat Search bar components

---
+++ src/Components/Search.jsx
@@ -0,0 +1,9 @@
+export default function Search() {
+
+    return (
+        <>
+            <input type="text" placeholder="Rechercher..." className="px-1 rounded-lg text-white"/>
+            <button className="bg-blue-500 text-white rounded-2xl px-1 py-1 ">Rechercher 🔍</button>
+        </>
+    )
+}

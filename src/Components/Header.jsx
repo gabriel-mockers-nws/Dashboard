0d604f70db471ac60d646fb537a8971215340835
gabriel-mockers-nws <gmockers@normandiewebschool.fr>
2025-09-11 11:27:11 +0200

    little correction

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -6,7 +6,7 @@ export const Header = () => {
         <nav className="flex gap-5 justify-end text-white bg-neutral-800 shadow-lg px-10 py-4 rounded-b-lg">
             <Link to="/">Vue d&apos;ensemble</Link>
             <Link to="/transactions">Transactions</Link>
-            <Link to="/clients">clients</Link>
+            <Link to="/clients">Clients</Link>
             <Link to="/account">Mon compte</Link>
             <Search />
         </nav>
